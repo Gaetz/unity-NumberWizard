01a2833f576a141242beb8bb034d65bae40f2f9b
Gaetz (Emilie-i3) <gaetz@rufflerim.com>
2017-04-24 11:12:55 +0200

    Game chosen number constraint

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -9,6 +9,11 @@ public class NumberWizard : MonoBehaviour {
         print("-= Welcome to Number Wizard =-");
         print("Pick a number in your head but don't tell me.");
 
+        const int MAX = 1000;
+        const int MIN = 1;
+
+        print("The number you choose must be between " + MIN.ToString() + " and "+ MAX.ToString() + ".");
+
     }
 
     // Update is called once per frame
